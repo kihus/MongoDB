3301752a40397cab921c2c8a006160a018d1056b
Bruno Nascimento <wintergamesk@gmail.com>
2025-11-14 09:38:11 -0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MongoDB.Driver;
+
+Console.WriteLine("Hello, World!");
+
+var client = new MongoClient("brunonlsctt_db_user:Pmz0Fy95yrwnJFub@interacaomongo.mpghkok.mongodb.net/");

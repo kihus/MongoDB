f2a188c95015d0e6a843a8f81c81c571fdf901a6
Bruno Nascimento <wintergamesk@gmail.com>
2025-11-14 09:39:19 -0300

    refactor: clean code

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,6 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-using MongoDB.Driver;
-
-Console.WriteLine("Hello, World!");
-
+﻿using MongoDB.Driver;
 var client = new MongoClient("brunonlsctt_db_user:Pmz0Fy95yrwnJFub@interacaomongo.mpghkok.mongodb.net/");
